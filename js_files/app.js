522019d68f010724a64bef4abd3c2562bb5fe194
madduxTim <timmaddux@Tims-MacBook-Pro.local>
2016-05-26 12:57:01 -0500

    Arbitrary stop point. Troubleshooting.

---
+++ js_files/app.js
@@ -0,0 +1,22 @@
+"use strict";
+var app = angular.module("ContactsApp", [])
+
+
+// THIS IS FOR SPLITTING UP THE HTML INTO SEPERATE FILES. DO LAST. 
+
+// app.config(function($routeProvider){
+//     $routeProvider.
+//         when("/items/list",{
+//             templateUrl: "partials/item-list.html",
+//             controller: "ItemListCtrl"
+//         }).
+//         when("/items/new", {
+//             templateUrl: "partials/item-new.html",
+//             controller: "ItemNewCtrl"
+//         }).
+//         when("/items/details", {
+//             templateUrl: "partials/item-details.html",
+//             controller: "ItemViewCtrl"
+//         }).
+//         otherwise("/items/list");
+// });
